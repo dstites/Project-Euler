cf6fff89986f79c9c7e960a42bdf009231d73ab0
dstites <dstites@autobase.net>
2011-04-26 08:19:54 -0400

    Problem 16 (and System.Numerics)

---
+++ Problems/Problem16.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Problems
+{
+    class Problem16 : BaseProblem
+    {
+        public override int ProblemId
+        {
+            get { return 16; }
+        }
+
+        public override string DoProblem()
+        {
+            BigInteger number = (BigInteger)Math.Pow(2, 1000);
+            var numberString = number.ToString();
+            var sum = numberString.Sum(t => Convert.ToInt16(t) - 48);
+            return sum.ToString();
+        }
+    }
+}
