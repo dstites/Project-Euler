56ed3e6809ba803e63cef1b5dc55ed3e3a7471fd
dstites <dstites@autobase.net>
2011-04-26 12:40:25 -0400

    Problems 13 and 20

---
+++ Problems/Problem11.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problems
+{
+    class Problem11 : BaseProblem
+    {
+        public override int ProblemId
+        {
+            get { return 11; }
+        }
+
+        public override string DoProblem()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ Problems/Problem20.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Problems
+{
+    class Problem20 : BaseProblem
+    {
+        public override int ProblemId
+        {
+            get { return 20; }
+        }
+
+        public override string DoProblem()
+        {
+            BigInteger product = 1;
+            for (int i = 100; i > 1; i--)
+            {
+                product *= i;
+            }
+            var sum = product.ToString().Sum(q=> Convert.ToInt16(q) - 48);
+            return sum.ToString();
+        }
+    }
+}
