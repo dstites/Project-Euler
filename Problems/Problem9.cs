35702114f91c052ff0b252c2501e5d2f477bd538
dstites <dstites@autobase.net>
2011-04-26 07:50:57 -0400

    Problem 9

---
+++ Problems/Problem9.cs
@@ -0,0 +1,26 @@
+﻿namespace Problems
+{
+    class Problem9 : BaseProblem
+    {
+        public override int ProblemId
+        {
+            get { return 9; }
+        }
+
+        public override string DoProblem()
+        {
+            for (var a = 1; a < 332; a++)
+            {
+                for (var b = a + 1; b > a && b < 1000; b++)
+                {
+                    var c = 1000 - b - a;
+                    if ((c > b) && (a + b + c == 1000) && (a*a + b*b == c*c))
+                    {
+                        return (a*b*c).ToString();
+                    }
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
