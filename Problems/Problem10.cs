d7eda391d22f1c93a24dce553e12e625bc4928d8
dstites <dstites@autobase.net>
2011-04-26 07:06:07 -0400

    Problem10.cs

---
+++ Problems/Problem10.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problems
+{
+    class Problem10 : BaseProblem
+    {
+        public override int ProblemId
+        {
+            get { return 10; }
+        }
+
+        public override string DoProblem()
+        {
+            const int max = 2000000;
+            var primes = new Dictionary<int, bool>(max);
+            for(var i = 3; i < max; i+= 2)
+            {
+                primes.Add(i, false);
+            }
+            var y = 3;
+            var sum = 2;
+            while(y < max)
+            {
+                for (var x = y*y; x < max; x += y)
+                {
+                    primes[x] = true;
+                }
+                sum += y;
+                y = primes.First(q => !q.Value && q.Key > y).Key;
+            }
+            return sum.ToString();
+        }
+    }
+}
